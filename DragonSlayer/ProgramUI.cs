7d677acead9d3abcc57d5884dcc961adf4e2b87a
Katelyn Hedlund <katelyn.renee1013@gmail.com>
2022-11-04 14:33:57 -0400

    added 4-6 moves + points

--- DragonSlayer/ProgramUI.cs
+++ DragonSlayer/ProgramUI.cs
@@ -2,6 +2,8 @@ using static System.Console;
 
 public class ProgramUI {
    private Knight knight = new Knight();
+   public int Points = 200;
+
 
    public void Run()
    {
@@ -46,7 +48,8 @@ public class ProgramUI {
     //   while (knight.IsAlive) //Checks to see if Knight is alive
     //   {
       WriteLine("Welcome to your journey!");
-      WriteLine("The princess has been taken by a dragon...You're a knight of the Round Table and you must SAVE HER! Survive the 3 castle levels to reach the dragon. Defeat the dragon to save the princess!!");
+      WriteLine("The princess has been taken by a dragon...You're a knight of the Round Table and you must SAVE HER! \n"
+      + "Survive the 3 castle levels to reach the dragon. Defeat the dragon to save the princess!!");
       WriteLine("Press any key to open the castle doors");
       ReadKey();
       KnightMove();
@@ -151,7 +154,8 @@ public class ProgramUI {
    private void ThirdMove()
    {
       System.Console.WriteLine("At the top of the staircase you see a bright gold treasure chest!!! You have received 250 points!");
-      // Points += 250;
+      Points= Points + 250;
+      System.Console.WriteLine("You now have " + Points + " points!");
       System.Console.WriteLine("Now you see there are two paths ahead: left and right. Choose a path.\n"
       + "1. left\n"
       + "2. right");
@@ -161,11 +165,12 @@ public class ProgramUI {
       switch (input)
       {
          case "1":
-         System.Console.WriteLine("You turn left. There is nothing there but cobwebs. Turn back!");
-         ThirdMove();
+         System.Console.WriteLine("You turn left. There is nothing there but cobwebs. Turn back! \n"
+         + "Once you turn back you take the other path.");
+          FourthMove();
          break;
          case "2":
-         System.Console.WriteLine("You turn right. You go around a hallway with some twists and turns. You reach another path.");
+         System.Console.WriteLine("You turn right. You go around a hallway with some twists and turns. You reach another two paths.");
             FourthMove();
             break;
             default:
@@ -174,7 +179,127 @@ public class ProgramUI {
       }
    }
 
-   private void FourthMove() { }
+   private void FourthMove() 
+   {
+      System.Console.WriteLine("Now you see there are two paths ahead: left and right. Choose a path.\n"
+      + "1. left\n"
+      + "2. right");
+
+      string? input = Console.ReadLine();
+
+      switch (input)
+      {
+         case "1":
+         System.Console.WriteLine("You turn left.");
+         ChestDeadMove();
+         break;
+         case "2":
+         System.Console.WriteLine("You turn right. After walking down a hall you come upon a man blocking your path.");
+            FifthMove();
+            break;
+            default:
+            System.Console.WriteLine("Invalid selection. Please try again!");
+            break;
+      }
+   }
+
+   private void ChestDeadMove() 
+   {
+      System.Console.WriteLine("After taking some turn you find a door covered in blood. Shall you go in or turn back... \n"
+      + "1. Open the door...\n"
+      + "2. Turn around and leave");
+
+      string? input = Console.ReadLine();
+
+      switch (input)
+      {
+         case "1":
+         System.Console.WriteLine("You find a Chest and gain 200 points! ");
+         Points= Points + 200;
+         System.Console.WriteLine("You now have " + Points + " points!\n"
+         + "Seeing there are no other paths you go back the way you came and take the other path.");
+         FifthMove();
+         break;
+         case "2":
+         System.Console.WriteLine("You go back and take the other path... After walking down a hall you come upon a man blocking your path.");
+            FifthMove();
+            break;
+            default:
+            System.Console.WriteLine("Invalid selection. Please try again!");
+            break;
+      }
+   }
+
+   private void FifthMove() 
+   {
+      System.Console.WriteLine("Wizard: Hello, pick a box and I will let you through.\n"
+      + "1. box 1 \n"
+      + "2. box 2 \n"
+      + "3. box 3");
+
+      string? input = Console.ReadLine();
+
+      switch (input)
+      {
+         case "1":
+         System.Console.WriteLine("Sorry, I must take 500 points. ");
+        Points= Points - 500;
+        System.Console.WriteLine("You now have " + Points + " points");
+          // add -500 points deduction - for most that means death
+         SixthMove();
+         break;
+         case "2":
+         System.Console.WriteLine("You found the holy sword worth 100 points. ! Let it aid you on your quest! ");
+         Points= Points + 100;
+         System.Console.WriteLine("You now have " + Points + " points");
+         // add +100 points 
+         SixthMove();
+         break;
+         case "3":
+         System.Console.WriteLine("Sorry, I must take 200 points.");
+         Points= Points - 200;
+         System.Console.WriteLine("You now have " + Points + " points!");
+          // add -200 points deduction
+         FourthMove();
+         break;
+            default:
+            System.Console.WriteLine("Invalid selection. Please try again!");
+            break;
+      }
+   }
+
+  private void SixthMove()
+   {
+      System.Console.WriteLine("You have reached two paths: straight and right. Choose a path.\n"
+      + "1. left\n"
+      + "2. right\n"
+      + "3. straight");
+
+      string? input = Console.ReadLine();
+      
+      switch (input)
+      {
+         case "1":
+         System.Console.WriteLine("You walk left ahead. It's a dead end. Turn back!");
+         SixthMove();
+         break;
+          case "2":
+         System.Console.WriteLine("You walk right ahead. It's a dead end. Turn back!");
+         SixthMove();
+         break;
+         case "3":
+         System.Console.WriteLine("You got straight. There is a stairway. You walk up the stairs. CONGRATULATIONS! You've made it to LEVEL THREE. There is something shiny...");
+          SixthMove();
+         // SeventhMove();
+         break;
+         default:
+         System.Console.WriteLine("Invalid selection. Please try again!");
+         break;
+      }
+   }
+
+
+
 
     // switch cases going through castle
 
